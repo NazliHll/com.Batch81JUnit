abaffd7cc989e5554e686474c2db5d7c46e74273
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-05-02 18:33:51 +0300

    02.05.2023

---
+++ src/test/java/day07_assertions/C03_Assertions.java
@@ -0,0 +1,10 @@
+package day07_assertions;
+
+public class C03_Assertions {
+     /*
+    http://www.automationpractice.com/index.php sayfasına gidelim
+    Sign in butonuna basalım
+    Email kutusuna @işareti olmayan bir mail adresi yazpı enter'a bastağımızda
+    "Invalid email address" uyarısı çıktığını test edelim
+     */
+}
