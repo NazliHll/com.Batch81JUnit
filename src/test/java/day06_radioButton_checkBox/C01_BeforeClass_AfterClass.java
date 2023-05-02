da41e2cbbe0b3cb9dec5558fe136cbbe975c501a
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-05-02 14:15:02 +0300

    02.05.2023

---
+++ src/test/java/day06_radioButton_checkBox/C01_BeforeClass_AfterClass.java
@@ -0,0 +1,36 @@
+package day06_radioButton_checkBox;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C01_BeforeClass_AfterClass {
+    WebDriver driver;
+    @BeforeClass
+    public void setup(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+    @AfterClass
+    public void tearDown(){
+        driver.close();
+    }
+    @Test
+    public void test01(){
+        driver.get("https://www.amazon.com");
+    }
+    @Test
+    public void test02(){
+        driver.get("https://www.instagram.com");
+    }
+    @Test
+    public void test03(){
+        driver.get("https://www.facebook.com");
+    }
+}
+
