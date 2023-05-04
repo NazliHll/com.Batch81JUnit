1898b78fef47c5a94dab74ffb2b091911ccb8f54
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-05-04 17:00:22 +0300

    04.05.2023

---
+++ src/test/java/day08__alerts_iframe_basicauth/C01_Alerts.java
@@ -0,0 +1,35 @@
+package day08__alerts_iframe_basicauth;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C01_Alerts {
+
+    WebDriver driver;
+    @Before
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+    }
+    @After
+    public void tearDown(){
+        //driver.quit();
+    }
+    @Test
+    public void test01(){
+        driver.get("https://hepsiburada.com");
+
+
+
+    }
+
+}
