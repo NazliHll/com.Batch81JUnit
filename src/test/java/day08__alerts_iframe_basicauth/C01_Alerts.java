d6e01eb3f687cf65c562323a9fea31fdbdbd6664
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-05-04 17:24:20 +0300

    04.05.2023

--- src/test/java/day08__alerts_iframe_basicauth/C01_Alerts.java
+++ src/test/java/day08__alerts_iframe_basicauth/C01_Alerts.java
@@ -2,9 +2,12 @@ package day08__alerts_iframe_basicauth;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
@@ -26,7 +29,18 @@ public class C01_Alerts {
     }
     @Test
     public void test01(){
-        driver.get("https://hepsiburada.com");
+        driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+        driver.findElement(By.xpath("//*[text()='Click for JS Alert']")).click();
+
+        driver.switchTo().alert().accept();
+
+        //result kisminda "You successfully clicked an alert" test edin
+
+        String expectedResult="You successfully clicked an alert";
+        WebElement sonucYaziElementi=driver.findElement(By.xpath("//p[@id='result']"));
+        String actualResult=sonucYaziElementi.getText();
+
+        Assert.assertEquals(expectedResult,actualResult);
 
 
 
