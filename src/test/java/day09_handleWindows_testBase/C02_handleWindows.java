0950883813735d106e2f7e44cf87b14d81ed8125
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-05-07 10:48:09 +0300

    07.05.2023

--- src/test/java/day09_handleWindows_testBase/C02_handleWindows.java
+++ src/test/java/day09_handleWindows_testBase/C02_handleWindows.java
@@ -2,9 +2,11 @@ package day09_handleWindows_testBase;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
@@ -28,17 +30,24 @@ public class C02_handleWindows {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-
     }
-
     @After
     public void tearDown() {
-        driver.quit();
+       // driver.quit();
     }
-
     @Test
     public void test1() {
         //1-amazona gidin.
         driver.get("https://amazon.com");
+        String istenenKelime="amazon";
+        String actualUrl=driver.getCurrentUrl();
+        Assert.assertTrue(actualUrl.contains(istenenKelime));
+        driver.switchTo().newWindow(WindowType.WINDOW);
+        driver.get("https://bestbuy.com");
+        String actualTitle= driver.getTitle();
+        String arananKelime="Best Buy";
+        Assert.assertTrue(actualTitle.contains(arananKelime));
+
+
     }
 }
