8b8d992d050d1e04c777b096babf7a71e036e39a
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-05-08 12:46:32 +0300

    08.05.2023

--- src/test/java/day10_actions/C06_KeyboardActions.java
+++ src/test/java/day10_actions/C06_KeyboardActions.java
@@ -1,5 +1,10 @@
 package day10_actions;
 
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import utilities.TestBase;
 
 public class C06_KeyboardActions extends TestBase {
@@ -9,4 +14,21 @@ public class C06_KeyboardActions extends TestBase {
     //geriye kalanları TAB ile dolasarak
     //formu doldursun
 
+    @Test
+    public void test01() throws InterruptedException {
+        driver.get("https://facebook.com");
+        driver.findElement(By.xpath("(//a[@role='button'])[2]")).click();
+        WebElement isimKutusu=driver.findElement(By.xpath("//input[@name='firstname']"));
+
+        Actions actions=new Actions(driver);
+        actions.click(isimKutusu)
+                .sendKeys("Nazlı")
+                .sendKeys(Keys.TAB)
+                .sendKeys("YAŞAR")
+                .sendKeys(Keys.TAB)
+                .sendKeys("nazli@gmail.com").perform();
+        Thread.sleep(5000);
+    }
 }
+
+
