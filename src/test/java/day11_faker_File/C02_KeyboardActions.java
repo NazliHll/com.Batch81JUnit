dc98ef0cd7357e049232ed8b0b95cf27da73c553
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-05-10 15:53:01 +0300

    10.05.2023

---
+++ src/test/java/day11_faker_File/C02_KeyboardActions.java
@@ -0,0 +1,20 @@
+package day11_faker_File;
+
+import org.junit.Test;
+import utilities.TestBase;
+
+public class C02_KeyboardActions extends TestBase {
+     /*
+        1.https://html.com/tags/iframe/ sayfasına gidelim
+        2.videoyu görecek kadar aşağıya in
+        3.videoyu izlemek için play tuşuna basın
+        4.videoyu çalıştırdığınızı test edin
+         */
+    @Test
+    public void test01(){
+        driver.get("https://html.com/tags/iframe/");
+
+    }
+
+
+}
