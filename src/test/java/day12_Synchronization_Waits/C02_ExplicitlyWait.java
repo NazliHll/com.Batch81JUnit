30057adf3c5f0bee891f8fee891d98d6c18541df
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-05-11 15:59:21 +0300

    11.05.2023

--- src/test/java/day12_Synchronization_Waits/C02_ExplicitlyWait.java
+++ src/test/java/day12_Synchronization_Waits/C02_ExplicitlyWait.java
@@ -1,6 +1,16 @@
 package day12_Synchronization_Waits;
 
-public class C02_ExplicitlyWait {
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import utilities.TestBase;
+
+import java.time.Duration;
+
+public class C02_ExplicitlyWait extends TestBase {
     /*bir class olusturun:EnableTest
     bir method olusturun:isEnabled()
     https://the-internet.herokuapp.com/dynamic_controls adresine gidin
@@ -9,4 +19,22 @@ public class C02_ExplicitlyWait {
     "It's enabled!" mesajinin goruntulendigini dogrulayin
     TextBox'in etkin oldugunu(enabled)dogrulayin
      */
+
+    @Test
+    public void enableTest() {
+        driver.get("https://the-internet.herokuapp.com/dynamic_controls");
+        WebElement enableKutusu= driver.findElement(By.xpath("//input[@type='text']"));
+        Assert.assertFalse(enableKutusu.isEnabled());
+        driver.findElement(By.xpath("//button[@onclick='swapInput()']")).click();
+        WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(15));
+        wait.until(ExpectedConditions.elementToBeClickable(enableKutusu));
+
+        WebElement itsEnable= driver.findElement(By.xpath("//p[text()=\"It's enabled!\"]"));
+        Assert.assertTrue(itsEnable.isDisplayed());
+        Assert.assertTrue(enableKutusu.isEnabled());
+
+
+
+
+    }
 }
