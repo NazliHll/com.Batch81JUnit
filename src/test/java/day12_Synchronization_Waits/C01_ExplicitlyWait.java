982929f5294a56b659434eb18f3e5abf2006ced2
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-05-11 15:40:57 +0300

    11.05.2023

--- src/test/java/day12_Synchronization_Waits/C01_ExplicitlyWait.java
+++ src/test/java/day12_Synchronization_Waits/C01_ExplicitlyWait.java
@@ -3,8 +3,13 @@ package day12_Synchronization_Waits;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import utilities.TestBase;
+
+import java.time.Duration;
 
 public class C01_ExplicitlyWait extends TestBase {
      /*
@@ -26,12 +31,31 @@ public class C01_ExplicitlyWait extends TestBase {
         driver.findElement(By.xpath("//button[text()=\"Add\"]")).click();
         WebElement itsBack= driver.findElement(By.xpath("//p[text()=\"It's back!\"]"));
         Assert.assertTrue(itsBack.isDisplayed());
+    }
+    @Test
+    public void explicitlyWaitTest(){
+        driver.get("https://the-internet.herokuapp.com/dynamic_controls");
+        driver.findElement(By.xpath("//button[@onclick='swapCheckbox()']")).click();
+        WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(15));
+        /*
+        WebElement itsGoneYazi= driver.findElement(By.xpath("//p[text()=\"It's gone!\"]"));
+        wait.until(ExpectedConditions.visibilityOf(itsGoneYazi));
+        Assert.assertTrue(itsGoneYazi.isDisplayed());
+
+        explicitWait itsgoneWE görünür olmasını beklerken o web elementinin locatini kullanarak assert yapmak
+        sorun olur ve exeption fırlatır. Henüz görülmeyen bir Webelementin locate edilmeside otamasyon için
+        mümkün olmayabilir.
+        (HTML kodları yazan developer farklı uygulamalar yapabilir).Bu durumda bekleme işlemi için explicitWait'i locate ile birlikte kullanırız.
+         */
+        WebElement itsGoneYazi=wait.until(ExpectedConditions.
+                                visibilityOfElementLocated(By.xpath("//p[text()=\"It's gone!\"]")));
+        Assert.assertTrue(itsGoneYazi.isDisplayed());
+
+        driver.findElement(By.xpath("//button[text()=\"Add\"]")).click();
 
 
-
-
-
-
+        WebElement itsBack= wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[text()=\"It's back!\"]")));
+        Assert.assertTrue(itsBack.isDisplayed());
 
 
     }
---
+++ src/test/java/day12_Synchronization_Waits/C02_ExplicitlyWait.java
@@ -0,0 +1,9 @@
+package day12_Synchronization_Waits;
+
+public class C02_ExplicitlyWait {
+    /*bir class olusturun:EnableTest
+    bir method olusturun:isEnabled()
+    https://the-internet.herokuapp.com/dynamic_controls adresine gidin
+    Textbox'in etkin olmadigini(enabled) dogrulayin
+     */
+}
