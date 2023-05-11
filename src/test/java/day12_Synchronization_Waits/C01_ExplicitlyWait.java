a43855b3a45df078219e512c5a6c9d239db280d4
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-05-11 14:49:48 +0300

    11.05.2023

---
+++ src/test/java/day12_Synchronization_Waits/C01_ExplicitlyWait.java
@@ -0,0 +1,14 @@
+package day12_Synchronization_Waits;
+
+public class C01_ExplicitlyWait {
+     /*
+    iki method oluşturun implicitWait(),explicitWait()
+    iki method için aşağıdaki adımları test edin
+
+    1.https://the-internet.herokuapp.com/dynamic_controls adresine gidin.
+    2.Remove butonuna basin.
+    3.“It’s gone!” mesajinin goruntulendigini dogrulayin.
+    4.Add buttonuna basin
+    5.It’s back mesajinin gorundugunu test edin
+     */
+}
