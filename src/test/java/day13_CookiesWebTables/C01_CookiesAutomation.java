f00534cb5ee4cb276ce6f7fde5ea074f0ec0e309
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-05-11 18:03:42 +0300

    11.05.2023

--- src/test/java/day13_CookiesWebTables/C01_CookiesAutomation.java
+++ src/test/java/day13_CookiesWebTables/C01_CookiesAutomation.java
@@ -1,6 +1,13 @@
 package day13_CookiesWebTables;
 
-public class C01_CookiesAutomation {
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.Cookie;
+import utilities.TestBase;
+
+import java.util.Set;
+
+public class C01_CookiesAutomation extends TestBase {
     /*
     1-Amazon anasayfaya gidin
     2-tum cookie’leri listeleyin
@@ -11,4 +18,45 @@ public class C01_CookiesAutomation {
     7-ismi skin olan cookie’yi silin ve silindigini test edin
     8-tum cookie’leri silin ve silindigini test edin
      */
+
+    @Test
+    public void cookiesTest() {
+        driver.get("https://www.amazon.com");
+        Set<Cookie> cookieSet=driver.manage().getCookies();
+        System.out.println(cookieSet);
+        int sayac=1;
+        for (Cookie each:cookieSet
+             ) {
+            System.out.println(sayac+".ci cookie: "+ each);
+            System.out.println("name: "+each.getName());
+            System.out.println("value: "+each.getValue());
+            sayac++;
+        }
+
+        int cookieSayisi=cookieSet.size();
+        Assert.assertTrue(cookieSayisi>5);
+
+        for (Cookie each:cookieSet
+             ) {
+            if (each.getName().equals("i18n-prefs")){
+                Assert.assertEquals("USD",each.getValue());
+            }
+        }
+        Cookie cookie=new Cookie("en sevdigim cookie","cikolatali");
+        driver.manage().addCookie(cookie);
+        cookieSet=driver.manage().getCookies();
+        sayac=1;
+        for (Cookie each:cookieSet
+        ) {
+            System.out.println(sayac+".ci cookie: "+ each);
+            sayac++;
+        }
+        Assert.assertTrue(cookieSet.contains(cookie));
+        driver.manage().deleteCookieNamed("skin");
+        Assert.assertFalse(cookieSet.contains("skin"));
+        driver.manage().deleteAllCookies();
+        cookieSet=driver.manage().getCookies();
+        Assert.assertTrue(cookieSet.isEmpty());
+
+    }
 }
--- src/test/java/utilities/TestBase.java
+++ src/test/java/utilities/TestBase.java
@@ -19,6 +19,6 @@ public abstract class TestBase {
     }
     @After
     public void tearDown(){
-      //  driver.close();
+        driver.close();
     }
 }
