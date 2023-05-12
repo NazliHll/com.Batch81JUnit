749a6b120984c65cf46f7fe07285fdd484c093a5
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-05-12 12:38:40 +0300

    12.05.2023

--- src/test/java/day13_CookiesWebTables/C02_WebTables.java
+++ src/test/java/day13_CookiesWebTables/C02_WebTables.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import utilities.TestBase;
 
+import java.util.List;
+
 public class C02_WebTables extends TestBase {
       /*
      login( ) metodun oluşturun ve oturum açın.
@@ -26,10 +28,40 @@ public class C02_WebTables extends TestBase {
 
         girisYap();
 
+        List<WebElement> sutunBasliklari=driver.findElements(By.xpath("//thead//tr[1]//th"));
+        System.out.println("sutun sayisi: "+sutunBasliklari.size());
+
+        WebElement tumBody=driver.findElement(By.xpath("//tbody"));
+        System.out.println(tumBody.getText());
+
+        List<WebElement>satirlarListesi=driver.findElements(By.xpath("//tbody//tr"));
+        System.out.println("satir sayisi: "+satirlarListesi.size());
+        for (WebElement each:satirlarListesi
+             ) {
+            System.out.println(each.getText());
+        }
+        List<WebElement>cellList=driver.findElements(By.xpath("//tbody//tr[4]//td"));
+        for (WebElement each:cellList
+             ) {
+            System.out.println(each.getText());
+        }
+        //email basligindaki tum elementleri(sutun) konsolda yazdirin.
+        List<WebElement>basliklar=driver.findElements(By.xpath("//thead//tr[1]//th"));
+        int emailSutunNo=0;
+        for (int i = 0; i < basliklar.size() ; i++) {
+            if (basliklar.get(i).getText().equals("Email")){
+                emailSutunNo=i;
+            }
+        }
+        List<WebElement>emailSutun=driver.findElements(By.xpath("//tbody//td["+(emailSutunNo+1)+"]"));
+        
+
     }
 
     public void girisYap() {
         driver.get("https://www.hotelmycamp.com");
+        driver.findElement(By.xpath("//button[@id='details-button']")).click();
+        driver.findElement(By.xpath("//a[@id='proceed-link']")).click();
         driver.findElement(By.linkText("Log in")).click();
         Actions actions =new Actions(driver);
         WebElement username=driver.findElement(By.id("UserName"));
