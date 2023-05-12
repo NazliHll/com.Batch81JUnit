d3e894ebc38f40a036ed4b1606bff517c982ef8d
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-05-12 11:33:20 +0300

    12.05.2023

--- src/test/java/day13_CookiesWebTables/C02_WebTables.java
+++ src/test/java/day13_CookiesWebTables/C02_WebTables.java
@@ -1,6 +1,13 @@
 package day13_CookiesWebTables;
 
-public class C02_WebTables {
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import utilities.TestBase;
+
+public class C02_WebTables extends TestBase {
       /*
      login( ) metodun oluşturun ve oturum açın.
      https://www.hotelmycamp.com admin/HotelRoomAdmin adresine gidin
@@ -14,4 +21,23 @@ public class C02_WebTables {
      4.satirdaki(row) elementleri konsolda yazdırın.
      */
 
+    @Test
+    public void webTables() {
+
+        girisYap();
+
+    }
+
+    public void girisYap() {
+        driver.get("https://www.hotelmycamp.com");
+        driver.findElement(By.linkText("Log in")).click();
+        Actions actions =new Actions(driver);
+        WebElement username=driver.findElement(By.id("UserName"));
+        actions.click(username).
+                sendKeys("manager").
+                sendKeys(Keys.TAB).
+                sendKeys("Manager1!").
+                sendKeys(Keys.ENTER).
+                perform();
+    }
 }
