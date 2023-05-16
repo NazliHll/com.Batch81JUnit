47ead03c70fe15407285f9ba8ffcedc3374eb9d7
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-05-16 16:55:34 +0300

    16.05.2023

--- src/test/java/day14_excel/C05_ReadExcel.java
+++ src/test/java/day14_excel/C05_ReadExcel.java
@@ -1,2 +1,35 @@
-package day14_excel;public class C05_ReadExcel {
+package day14_excel;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class C05_ReadExcel {
+    @Test
+    public void readExcelTest() throws IOException {
+        String dosyaYolu="src/resources/ulkeler.xlsx";
+        FileInputStream fis=new FileInputStream(dosyaYolu);
+        Workbook workbook= WorkbookFactory.create(fis);
+
+        //sayfa 2'ye gidip
+        //satır sayısının 24, kullanılan satır sayısının 12 olduğunu test edin.
+
+        int sonSatirIndex= workbook
+                .getSheet("Sayfa2")
+                .getLastRowNum();
+        int expectedSatirSayisi=24;
+        Assert.assertEquals(expectedSatirSayisi,sonSatirIndex+1);
+
+        int kullanilanSatirSayisi=workbook
+                .getSheet("Sayfa2")
+                .getPhysicalNumberOfRows();
+        int expectedKullanilanSatirSayisi=12;
+        Assert.assertEquals(expectedKullanilanSatirSayisi,kullanilanSatirSayisi);
+
+    }
 }
--- src/test/java/day14_excel/C06_ReadExcel.java
+++ src/test/java/day14_excel/C06_ReadExcel.java
@@ -1,2 +1,44 @@
-package day14_excel;public class C06_ReadExcel {
+package day14_excel;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class C06_ReadExcel {
+    @Test
+    public void readExcelTest() throws IOException {
+         /*
+    Excel dosyasındaki tüm verileri clasımıza alıp bir Java objesine store edelim
+    böylece her seferinde excel'e ulaşıp satır,sutun vs... uğraşmayalım
+    Bunun için map yapıcaz : key ve valuelere ihtiyacımız var
+
+    Database yapısında olan exceli koyabileceğimiz en uygun Java objesi Map
+     */
+        Map<String,String> ulkelerMap=new HashMap<>();
+        String dosyaYolu="src/resources/ulkeler.xlsx";
+        FileInputStream fis=new FileInputStream(dosyaYolu);
+        Workbook workbook= WorkbookFactory.create(fis);
+        int sonSatirIndex=workbook.getSheet("Sayfa1").getLastRowNum();
+
+        for (int i = 0; i <=sonSatirIndex ; i++) {
+            String key=workbook.getSheet("Sayfa1")
+                    .getRow(i).getCell(0).toString();
+
+            String value=workbook.getSheet("Sayfa1").getRow(i).getCell(1).toString()
+                    +", "
+                    +workbook.getSheet("Sayfa1").getRow(i).getCell(2).toString()
+                    +", "
+                    +workbook.getSheet("Sayfa1").getRow(i).getCell(3).toString();
+            ulkelerMap.put(key,value);
+
+        }
+        System.out.println(ulkelerMap);
+
+
+    }
 }
