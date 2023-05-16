acd7be850206769589aa5c6329950813e2a42f36
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-05-16 15:30:32 +0300

    16.05.2023

--- src/test/java/day14_excel/C02_ReadExcel.java
+++ src/test/java/day14_excel/C02_ReadExcel.java
@@ -1,10 +1,16 @@
 package day14_excel;
 
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class C02_ReadExcel {
     @Test
-    public void readExcelTest() {
+    public void readExcelTest() throws IOException {
     /* Dosya yolunu bir String degiskene atayalim
          FileInputStream objesi olusturup,parametre olarak dosya yolunu girelim
          Workbook objesi olusturalim,parameter olarak fileInputStream objesini girelim
@@ -12,6 +18,15 @@ public class C02_ReadExcel {
          Row objesi olusturun sheet.getRow(index)
         Cell objesi olusturun row.getCell(index)
         3.index'deki satırın  3. indexindeki datanın Cezayir olduğunu test edin.*/
+
+        String dosyaYolu="src/resources/ulkeler.xlsx";
+        FileInputStream fis=new FileInputStream(dosyaYolu);
+        Workbook workbook= WorkbookFactory.create(fis);
+        String actualData=workbook.
+                          getSheet("Sayfa1").
+                          getRow(3).
+                          getCell(3).toString();
+
 
     }
 }
