5c18dc28f736a96451303a3920ca183cc5477526
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-05-16 11:25:23 +0300

    16.05.2023

--- src/test/java/day13_CookiesWebTables/C03_WebTables.java
+++ src/test/java/day13_CookiesWebTables/C03_WebTables.java
@@ -1,4 +1,43 @@
 package day13_CookiesWebTables;
 
-public class C03_WebTables {
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import utilities.TestBase;
+
+public class C03_WebTables extends TestBase {
+    @Test
+    public void dinamikYazici() {
+
+
+        //onceki classtaki adrese gidip
+        //girisYap methodunu kullanarak sayfaya giris yapin
+        //input olarak verilen satir sayisi ve sutun sayisina sahip
+        //celldeki texti yazdirin
+
+        int satir = 3;
+        int sutun = 5;
+        girisYap();
+
+        WebElement arananCell=driver.findElement(By.xpath("//tbody//tr[3]//td[5]"));
+        System.out.println(arananCell.getText());
+
+
+    }
+    public void girisYap() {
+        driver.get("https://www.hotelmycamp.com");
+        driver.findElement(By.xpath("//button[@id='details-button']")).click();
+        driver.findElement(By.xpath("//a[@id='proceed-link']")).click();
+        driver.findElement(By.linkText("Log in")).click();
+        Actions actions =new Actions(driver);
+        WebElement username=driver.findElement(By.id("UserName"));
+        actions.click(username).
+                sendKeys("manager").
+                sendKeys(Keys.TAB).
+                sendKeys("Manager1!").
+                sendKeys(Keys.ENTER).
+                perform();
+    }
 }
