c6ac0bb43c384109681615dda1b168157f79c8b8
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-05-17 11:04:50 +0300

    17.05.2023

---
+++ src/test/java/day15_writeExcel_screenShot/C03_IstenenWebElementSS.java
@@ -0,0 +1,11 @@
+package day15_writeExcel_screenShot;
+
+import org.junit.Test;
+import utilities.TestBase;
+
+public class C03_IstenenWebElementSS extends TestBase {
+    @Test
+    public void webElementSS() {
+
+    }
+}
