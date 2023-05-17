b450d5f7d48dc3821fae6c161e1d0065eaeb95fd
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-05-17 11:00:28 +0300

    17.05.2023

--- src/test/java/day15_writeExcel_screenShot/C02_TumSayfaScreenShot.java
+++ src/test/java/day15_writeExcel_screenShot/C02_TumSayfaScreenShot.java
@@ -8,13 +8,21 @@ import utilities.TestBase;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class C02_TumSayfaScreenShot extends TestBase {
     @Test
     public void tumSayfa() throws IOException {
         driver.get("https://www.amazon.com");
         TakesScreenshot ts= (TakesScreenshot) driver;
-        File tumSayfaResim=new File("target/ekranGoruntuleri/tumSayfa.jpeg");
+        LocalDateTime date=LocalDateTime.now();
+        System.out.println(date);
+        DateTimeFormatter dtf=DateTimeFormatter.ofPattern("YYMMddHHmmss");
+        String tarih=date.format(dtf);
+        System.out.println(tarih);
+        File tumSayfaResim=new File("target/ekranGoruntuleri/tumSayfa"+tarih+".jpeg");
         File resimGecici=ts.getScreenshotAs(OutputType.FILE);
         FileUtils.copyFile(resimGecici,tumSayfaResim);
     }
